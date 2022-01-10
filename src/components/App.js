1c42cc595d930f09d725ca21c62677aaafeb35af
Tenzin Jorden <tenden96@gmail.com>
2022-01-10 14:38:46 -0800

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -18,13 +18,21 @@ function Home() {
 
 {/* write an <About> component here */}
 
+function About() {
+  return(
+    <div id ="about">
+      <h1>About</h1>
+      </div>
+  );
+}
+
 
 function App() {
-  return (
+  return(
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
